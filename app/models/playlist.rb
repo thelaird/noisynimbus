dc908a35cf854ad53e4b65a1e2d08415392e3190
thelaird <hey@thisis.lt>
2015-06-18 11:33:50 -0700

    Order playlists by create_at

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -14,6 +14,8 @@ class Playlist < ActiveRecord::Base
   validates :title, :user_id, presence: true
   validates :title, length: { maximum: 30 }
 
+  default_scope { order(:created_at) }
+
   has_many :playlist_items, dependent: :destroy
   has_many :songs, through: :playlist_items
   belongs_to :user
