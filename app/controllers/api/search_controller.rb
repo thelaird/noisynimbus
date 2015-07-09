ad6a95b2d74ee28cb35a2de5a1637e8c4fc4bd9a
Laird Tuttle <hey@thisis.lt>
2015-07-08 22:32:54 -0700

    Order search results

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -2,8 +2,8 @@ module Api
   class SearchController < ApiController
 
     def index
-      @songs_by_artist = Song.where("artist ILIKE ?", "%#{search_params[:query]}%")
-      @songs_by_title = Song.where("title ILIKE ?", "%#{search_params[:query]}%")
+      @songs_by_artist = Song.where("artist ILIKE ?", "%#{search_params[:query]}%").order('created_at desc')
+      @songs_by_title = Song.where("title ILIKE ?", "%#{search_params[:query]}%").order('created_at desc')
       @tags = Tag.where("text ILIKE ?", "%#{search_params[:query]}%")
       @users = User.where("username ILIKE ?", "%#{search_params[:query]}%")
       render :index
